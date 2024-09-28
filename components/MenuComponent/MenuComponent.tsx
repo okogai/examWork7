b60388a79bd8034f420285df572e69c42f2485b9
Oleg <evolrn89@gmail.com>
2024-09-28 17:51:37 +0600

    Created component MenuComponent to display products in menu.

---
+++ components/MenuComponent/MenuComponent.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { MenuItem } from '../../src/types';
+import './MenuComponent.css';
+
+interface MenuProps {
+    menuItems: MenuItem[];
+    addItem: (item: MenuItem) => void;
+}
+
+const MenuComponent: React.FC<MenuProps> = ({ menuItems, addItem }) => {
+    return (
+        <div className='menu'>
+            <h2 className='menu-title'>Add Items</h2>
+            {menuItems.map(item => (
+                <div key={item.id} className="menu-item">
+                    <img src={item.image} alt={item.name} style={{ width: '150px' }} />
+                    <button onClick={() => addItem(item)}>
+                        {item.name} - {item.price}
+                    </button>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default MenuComponent;
